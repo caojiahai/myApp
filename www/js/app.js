4ef3669278a66fb407a252dcbfed843470c574ea
caojiahai <1041630444@qq.com>
2017-03-17 17:22:03 +0800

    主页双击退出app

--- www/js/app.js
+++ www/js/app.js
@@ -1,19 +1,40 @@
-// Ionic Starter App
-var app = angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','ngCordova'])
+var app = angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngCordova'])
 var appCtrl = angular.module('starter.controllers', [])
 var appService = angular.module('starter.services', [])
 var API_URL = 'http://115.29.149.164/mobile-API/';
+var paths = ['/tab/home'];
 //var API_URL = 'http://192.168.1.235:80/mobile-API/';
-app.run(function($ionicPlatform) {
-	$ionicPlatform.ready(function() {
-		if(window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-			cordova.plugins.Keyboard.disableScroll(true);
+app.run(function($ionicPlatform, $rootScope, Toast, $location) {
+		$ionicPlatform.ready(function() {
+				if(window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+						cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+						cordova.plugins.Keyboard.disableScroll(true);
+				}
+				if(window.StatusBar) {
+				// org.apache.cordova.statusbar required
+						StatusBar.styleDefault();
+				}
+		});
 
-		}
-		if(window.StatusBar) {
-			// org.apache.cordova.statusbar required
-			StatusBar.styleDefault();
-		}
-	});
+		$ionicPlatform.registerBackButtonAction(function(e) {
+				e.preventDefault();
+				var path = $location.path();
+				var isPath = false;
+            angular.forEach(paths, function (data) {
+                if (data.indexOf(path) != -1)
+                    isPath = true;
+            });
+            if (isPath) {
+            	if($rootScope.backButtonPressedOnceToExit) {
+									ionic.Platform.exitApp();
+							} else {
+									$rootScope.backButtonPressedOnceToExit = true;
+									Toast.show('再按一次退出系统');
+									setTimeout(function() {
+											$rootScope.backButtonPressedOnceToExit = false;
+									}, 2000);
+							}
+            }
+				
+		}, 101);
 })
--- www/templates/home/tab-homeCtrl.js
+++ www/templates/home/tab-homeCtrl.js
@@ -1,32 +1,76 @@
 /**
  * Created by cjh on 2017/3/10.
  */
-appCtrl.controller('HomeCtrl', function($scope, $cordovaGeolocation, Toast) {
+appCtrl.controller('HomeCtrl', function($scope,$cordovaGeolocation,Toast ,$cordovaContacts,
+	$cordovaCamera,$cordovaNetwork,$rootScope,$cordovaVibration) {
 	
-	var posOptions = {
-		timeout: 10000,
-		enableHighAccuracy: false
-	};
+//	var posOptions = {
+//		timeout: 10000,
+//		enableHighAccuracy: false
+//	};
 	
-	$cordovaGeolocation
-		.getCurrentPosition(posOptions)
-		.then(function(position) {
-			var lat = position.coords.latitude
-			var long = position.coords.longitude
-			Toast.show('获取地理位置成功')
-		}, function(err) {
-		    switch(err.code){
-		        case err.TIMEOUT :
-		        	log = " 连接超时，请重试 ";
-		            break;
-		        case err.PERMISSION_DENIED :
-		        	log = " 您拒绝了使用位置共享服务，查询已取消 ";
-		            break;
-		        case err.POSITION_UNAVAILABLE :  
-		        	log = " 亲爱的火星网友，非常抱歉，我们暂时无法为您所在的星球提供位置服务 ";
-		            break;
-		    }
-		    Toast.show(log,3000)
-		});
+	//获取地理位置--ok
+//	$cordovaGeolocation
+//		.getCurrentPosition(posOptions)
+//		.then(function(position) {
+//			var lat = position.coords.latitude
+//			var long = position.coords.longitude
+//			Toast.show('获取地理位置成功')
+//		}, function(err) {
+//		    switch(err.code){
+//		        case err.TIMEOUT :
+//		        	log = " 连接超时，请重试 ";
+//		            break;
+//		        case err.PERMISSION_DENIED :
+//		        	log = " 您拒绝了使用位置共享服务，查询已取消 ";
+//		            break;
+//		        case err.POSITION_UNAVAILABLE :  
+//		        	log = " 亲爱的火星网友，非常抱歉，我们暂时无法为您所在的星球提供位置服务 ";
+//		            break;
+//		    }
+//		    Toast.show(log,3000)
+//		});
+		
+	//获取手机联系人--未通过
+//	$cordovaContacts.find().then(function(allContacts) { //omitting parameter to .find() causes all contacts to be returned
+//    	console.log(allContacts)
+// 	})
+
+
+//相机--ok
+//  var options = {
+//    quality: 50,
+//    destinationType: Camera.DestinationType.DATA_URL,
+//    sourceType: Camera.PictureSourceType.CAMERA,
+//    allowEdit: true,
+//    encodingType: Camera.EncodingType.JPEG,
+//    targetWidth: 100,
+//    targetHeight: 100,
+//    popoverOptions: CameraPopoverOptions,
+//    saveToPhotoAlbum: false,
+//    correctOrientation:true
+//  };
+//
+//  $cordovaCamera.getPicture(options).then(function(imageData) {
+//    console.log(imageData)
+//  }, function(err) {
+//    console.log(err)
+//  });
+
+
+//  // 监听手机网络在线事件--ok
+//  $rootScope.$on('$cordovaNetwork:online', function(event, networkState){
+//    var onlineState = networkState;
+//    console.log(onlineState)
+//  })
+//
+//  // 监听手机网络离线事件--ok
+//  $rootScope.$on('$cordovaNetwork:offline', function(event, networkState){
+//    var offlineState = networkState;
+//    console.log(offlineState)
+//  })
+
+//震动--ok
+//	$cordovaVibration.vibrate(100);
 
 })
