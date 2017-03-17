c2e569ef7c2a21225666540d4ab6e73311f9ebcd
caojiahai <1041630444@qq.com>
2017-03-17 15:06:31 +0800

    bc

--- www/js/app.js
+++ www/js/app.js
@@ -1,21 +1,19 @@
 // Ionic Starter App
-var app = angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+var app = angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter.services'])
 var appCtrl = angular.module('starter.controllers', [])
 var appService = angular.module('starter.services', [])
 var API_URL = 'http://115.29.149.164/mobile-API/';
 //var API_URL = 'http://192.168.1.235:80/mobile-API/';
 app.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+	$ionicPlatform.ready(function() {
+		if(window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+			cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+		}
+		if(window.StatusBar) {
+			// org.apache.cordova.statusbar required
+			StatusBar.styleDefault();
+		}
+	});
 })
-
-
